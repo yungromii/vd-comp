e9206db83e6e3b8293224a25a4c0e62c6ed12a0e
yungromii <hamsterdoll@snu.ac.kr>
2025-10-14 15:29:40 +0900

    brush

--- brush/sketch.js
+++ brush/sketch.js
@@ -1,7 +1,39 @@
+let mic, micOn = false, btn;
+
+function preload() {
+  brushImg = loadImage('yungromii.png'); // replace 'brush.png' with your actual file name
+}
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+
+  imageMode(CENTER); // so the brush centers under the curso
+  mic = new p5.AudioIn();
+  mic.start(); // start mic input
+  btn = createButton('Mic: OFF');
+  btn.position(10, 10);
+  btn.mousePressed(toggleMic);
 }
 
 function draw() {
-  background(220);
+  if (mouseIsPressed) {
+    let level = micOn ? mic.getLevel() : 0;
+    let w = 200 + level * 800; // adjust multiplier to see size change more clearly
+    image(brushImg, mouseX, mouseY, w, w);
+  }
+}
+
+function toggleMic() {
+  if (!micOn) {
+    getAudioContext().resume();
+    mic.start();
+    micOn = true;
+    btn.html('Mic: ON');
+  } else {
+    mic.stop();
+    micOn = false;
+    btn.html('Mic: OFF');
+  }
 }
